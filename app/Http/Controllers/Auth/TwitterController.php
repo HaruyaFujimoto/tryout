526d54ef0b63266131c60206f87dfae899ff4803
hal822 <hal2cilu@gmail.com>
2019-03-19 16:47:31 +0900

    login isues

--- app/Http/Controllers/Auth/TwitterController.php
+++ app/Http/Controllers/Auth/TwitterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Socialite;
 use App\User;
+use Auth;
 
 class TwitterController extends Controller
 {
--- database/migrations/2019_03_19_161318_create_plan_skill_table.php
+++ database/migrations/2019_03_19_161318_create_plan_skill_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePlanSkillTalbe extends Migration
+class CreatePlanSkillTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreatePlanSkillTalbe extends Migration
      */
     public function up()
     {
-        Schema::create('plan_skill_talbe', function (Blueprint $table) {
+        Schema::create('plan_skill', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
-            $table->integer('plan_id');
-            $table->integer('skill_id');
         });
     }
 
@@ -28,6 +26,6 @@ class CreatePlanSkillTalbe extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('plan_skill_talbe');
+        Schema::dropIfExists('plan_skill');
     }
 }
